5398064c2ebf8fbade268f083647e44d592045c0
cd27r44cny-dotcom <cd27r44cny@privaterelay.appleid.com>
2026-09-22 22:19:24 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='sr-tool-v6.4.7-20260922';
+const CACHE='sr-tool-v6.4.8-20260922';
 const CORE=['./','./index.html','./invoice.html','./invoice.js','./deb-rechnung-vorlage.png','./manifest.webmanifest'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE).catch(()=>{})))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
